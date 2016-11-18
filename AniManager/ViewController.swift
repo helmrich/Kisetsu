e51744df4047fdc3a1b31aeaf16014ba621376ff
Tobias Helmrich <me@tobias-helmrich.de>
2016-11-18 19:55:21 +0100

    Initial Commit

---
+++ AniManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AniManager
+//
+//  Created by Tobias Helmrich on 18.11.16.
+//  Copyright © 2016 Tobias Helmrich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

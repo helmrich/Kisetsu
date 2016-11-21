42bfb376fea2be2f83eaac0c0ccfebda6c0a06f0
Tobias Helmrich <me@tobias-helmrich.de>
2016-11-21 12:23:29 +0100

    feat: Start implementing the AniList client
    - Create struct that holds all the client's constants
    - Create AniListClient class that should contain all the actual client code

---
+++ AniManager/AniListClient.swift
@@ -0,0 +1,35 @@
+//
+//  AniListClient.swift
+//  AniManager
+//
+//  Created by Tobias Helmrich on 21.11.16.
+//  Copyright © 2016 Tobias Helmrich. All rights reserved.
+//
+
+import Foundation
+
+class AniListClient {
+    
+    static let shared = AniListClient()
+    
+    fileprivate init() {}
+
+    // MARK: - Helper methods
+    static func createAniListUrl(withPath path: String, andParameters parameters: [String:Any]) -> URL? {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = AniListConstant.URL.scheme
+        urlComponents.host = AniListConstant.URL.host
+        urlComponents.path = "\(AniListConstant.URL.basePath)\(path)"
+        
+        var queryItems = [URLQueryItem]()
+        for (parameterName, parameterValue) in parameters {
+            let queryItem = URLQueryItem(name: parameterName, value: "\(parameterValue)")
+            queryItems.append(queryItem)
+        }
+        
+        urlComponents.queryItems = queryItems
+        
+        return urlComponents.url
+        
+    }
+}
---
+++ AniManager/AniListConstant.swift
@@ -0,0 +1,47 @@
+//
+//  AniListConstant.swift
+//  AniManager
+//
+//  Created by Tobias Helmrich on 20.11.16.
+//  Copyright © 2016 Tobias Helmrich. All rights reserved.
+//
+
+import Foundation
+
+struct AniListConstant {
+    struct Account {
+        static let clientId = "tobe-pikrk"
+        static let clientSecret = "OUlu5I6VyOvs7jaTLJgS4h"
+    }
+    
+    struct URL {
+        static let scheme = "https"
+        static let host = "anilist.co"
+        static let basePath = "/api/"
+    }
+    
+    struct Path {
+        struct Authentication {
+            static let authorize = "auth/authorize"
+            static let accessToken = "auth/access_token"
+        }
+    }
+    
+    struct ParameterKey {
+        struct Authentication {
+            static let grantType = "grant_type"
+            static let clientId = "client_id"
+            static let redirectUri = "redirect_uri"
+            static let responseType = "response_type"
+        }
+    }
+    
+    struct ParameterValue {
+        struct Authentication {
+            static let grantType = "authorization_code"
+            static let redirectUri = "AniManager://"
+            static let responseType = "code"
+        }
+    }
+    
+}

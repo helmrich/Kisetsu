813b3a8a1475fb252cfd1088756c64d5b1194dd5
Tobias Helmrich <me@tobias-helmrich.de>
2016-11-22 20:35:45 +0100

    feat: Add AccessToken struct

---
+++ AniManager/AccessToken.swift
@@ -0,0 +1,15 @@
+//
+//  AccessToken.swift
+//  AniManager
+//
+//  Created by Tobias Helmrich on 21.11.16.
+//  Copyright © 2016 Tobias Helmrich. All rights reserved.
+//
+
+import Foundation
+
+struct AccessToken {
+    let accessTokenValue: String
+    let tokenType: String
+    let expirationTimestamp: Int
+}

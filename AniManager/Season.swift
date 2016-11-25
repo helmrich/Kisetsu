c99add26aa10641a94f6a6dead849f9b7aee2d52
Tobias Helmrich <me@tobias-helmrich.de>
2016-11-25 20:10:19 +0100

    chore: Add Season enum

---
+++ AniManager/Season.swift
@@ -0,0 +1,13 @@
+//
+//  Season.swift
+//  AniManager
+//
+//  Created by Tobias Helmrich on 24.11.16.
+//  Copyright © 2016 Tobias Helmrich. All rights reserved.
+//
+
+import Foundation
+
+enum Season: String {
+    case winter, spring, summer, fall
+}

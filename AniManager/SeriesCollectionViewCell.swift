8a06b46277b1a829e2b626cc4b22940d6f625681
Tobias Helmrich <me@tobias-helmrich.de>
2016-11-27 20:17:46 +0100

    feat: Add custom series collection view cell for BrowseViewController's collection view

---
+++ AniManager/SeriesCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SeriesCollectionViewCell.swift
+//  AniManager
+//
+//  Created by Tobias Helmrich on 25.11.16.
+//  Copyright © 2016 Tobias Helmrich. All rights reserved.
+//
+
+import UIKit
+
+class SeriesCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageOverlay: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+}

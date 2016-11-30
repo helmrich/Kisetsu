c0cd8ad743981079a535a5a81d1c01b9959cda6d
Tobias Helmrich <me@tobias-helmrich.de>
2016-11-30 21:32:22 +0100

    feat: Start implementing series detail view controller
    - Start building the interface in Storyboard
    - Create custom cells (description, basic information)
    - Add icons (back arrow, heart)

---
+++ AniManager/DescriptionTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DescriptionTableViewCell.swift
+//  AniManager
+//
+//  Created by Tobias Helmrich on 30.11.16.
+//  Copyright © 2016 Tobias Helmrich. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
+
+}
---
+++ AniManager/SeriesDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SeriesDetailViewController.swift
+//  AniManager
+//
+//  Created by Tobias Helmrich on 29.11.16.
+//  Copyright © 2016 Tobias Helmrich. All rights reserved.
+//
+
+import UIKit
+
+class SeriesDetailViewController: UIViewController {
+
+    
+
+}

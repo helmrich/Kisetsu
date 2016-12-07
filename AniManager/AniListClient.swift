7690144b26f86703874329150083fd807544d6f3
Tobias Helmrich <me@tobias-helmrich.de>
2016-12-08 00:08:30 +0100

    fix: Add isAccessTokenValid and validateAccessToken methods
    When the access token expired while using the application and a request was made,
    the app always shows errors because requests to the AniList API don't work without a
    valid access token, thus the app had to be restarted in order to work again.
    
    Those methods should be used in order to check if a valid access token is available on
    each request and if not, request a new access token with the refresh token.

--- AniManager/AniListClient.swift
+++ AniManager/AniListClient.swift
@@ -94,175 +94,206 @@ class AniListClient {
     
     func getSeriesList(fromPage page: Int = 1, ofType seriesType: SeriesType, andParameters parameters: [String:Any], completionHandlerForSeriesList: @escaping (_ seriesList: [Series]?, _ errorMessage: String?) -> Void) {
         
-        let replacingPairs = [
-            AniListConstant.Path.Placeholder.seriesType: seriesType.rawValue
-        ]
-        
-        let path = replacePlaceholders(inPath: AniListConstant.Path.SeriesGet.browse, withReplacingPairs: replacingPairs)
-        
-        var allParameters = parameters
-        allParameters[AniListConstant.ParameterKey.Browse.page] = page
-        
-        guard let url = createAniListUrl(withPath: path, andParameters: allParameters) else {
-            completionHandlerForSeriesList(nil, "Couldn't create AniList URL")
-            return
-        }
-
-        let request = NSMutableURLRequest(url: url)
-        request.addValue(AniListConstant.HeaderFieldValue.contentType, forHTTPHeaderField: AniListConstant.HeaderFieldName.contentType)
-        request.addValue("Bearer \(UserDefaults.standard.string(forKey: "accessToken")!)", forHTTPHeaderField: AniListConstant.HeaderFieldName.authorization)
-        
-        let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
-            
-            if let errorMessage = self.checkDataTaskResponseForError(data: data, response: response, error: error) {
-                completionHandlerForSeriesList(nil, errorMessage)
+        validateAccessToken { (errorMessage) in
+            guard errorMessage == nil else {
+                completionHandlerForSeriesList(nil, errorMessage!)
                 return
             }
             
-            let data = data!
+            let replacingPairs = [
+                AniListConstant.Path.Placeholder.seriesType: seriesType.rawValue
+            ]
             
-            guard let jsonObject = self.deserializeJson(fromData: data) else {
-                completionHandlerForSeriesList(nil, "Couldn't deserialize data into a JSON object")
+            let path = self.replacePlaceholders(inPath: AniListConstant.Path.SeriesGet.browse, withReplacingPairs: replacingPairs)
+            
+            var allParameters = parameters
+            allParameters[AniListConstant.ParameterKey.Browse.page] = page
+            
+            guard let url = self.createAniListUrl(withPath: path, andParameters: allParameters) else {
+                completionHandlerForSeriesList(nil, "Couldn't create AniList URL")
                 return
             }
             
-            guard let rawSeriesList = jsonObject as? [[String:Any]] else {
-                completionHandlerForSeriesList(nil, "Couldn't cast JSON to array of dictionaries")
-                return
-            }
+            let request = NSMutableURLRequest(url: url)
+            request.addValue(AniListConstant.HeaderFieldValue.contentType, forHTTPHeaderField: AniListConstant.HeaderFieldName.contentType)
+            request.addValue("Bearer \(UserDefaults.standard.string(forKey: "accessToken")!)", forHTTPHeaderField: AniListConstant.HeaderFieldName.authorization)
             
-            typealias seriesKey = AniListConstant.ResponseKey.Series
-            
-            var seriesList = [Series]()
-            if seriesType.rawValue == SeriesType.anime.rawValue {
-                seriesList = seriesList as! [AnimeSeries]
-                for series in rawSeriesList {
-                    if let animeSeries = AnimeSeries(fromDictionary: series) {
-                        seriesList.append(animeSeries)
-                    } else {
-                        print("Couldn't create anime series")
+            let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
+                
+                if let errorMessage = self.checkDataTaskResponseForError(data: data, response: response, error: error) {
+                    completionHandlerForSeriesList(nil, errorMessage)
+                    return
+                }
+                
+                let data = data!
+                
+                guard let jsonObject = self.deserializeJson(fromData: data) else {
+                    completionHandlerForSeriesList(nil, "Couldn't deserialize data into a JSON object")
+                    return
+                }
+                
+                guard let rawSeriesList = jsonObject as? [[String:Any]] else {
+                    completionHandlerForSeriesList(nil, "Couldn't cast JSON to array of dictionaries")
+                    return
+                }
+                
+                typealias seriesKey = AniListConstant.ResponseKey.Series
+                
+                var seriesList = [Series]()
+                if seriesType.rawValue == SeriesType.anime.rawValue {
+                    seriesList = seriesList as! [AnimeSeries]
+                    for series in rawSeriesList {
+                        if let animeSeries = AnimeSeries(fromDictionary: series) {
+                            seriesList.append(animeSeries)
+                        } else {
+                            print("Couldn't create anime series")
+                        }
+                    }
+                } else {
+                    seriesList = seriesList as! [MangaSeries]
+                    for series in rawSeriesList {
+                        if let mangaSeries = MangaSeries(fromDictionary: series) {
+                            seriesList.append(mangaSeries)
+                        } else {
+                            print("Couldn't create manga series")
+                        }
                     }
                 }
-            } else {
-                seriesList = seriesList as! [MangaSeries]
-                for series in rawSeriesList {
-                    if let mangaSeries = MangaSeries(fromDictionary: series) {
-                        seriesList.append(mangaSeries)
-                    } else {
-                        print("Couldn't create manga series")
-                    }
-                }
+                
+                completionHandlerForSeriesList(seriesList, nil)
+                
             }
             
-            completionHandlerForSeriesList(seriesList, nil)
+            task.resume()
             
         }
         
-        task.resume()
         
     }
     
     func getSingleSeries(ofType seriesType: SeriesType, withId id: Int, completionHandlerForSeries: @escaping (_ series: Series?, _ errorMessage: String?) -> Void) {
-        let replacingPairs = [
-            AniListConstant.Path.Placeholder.seriesType: seriesType.rawValue,
-            AniListConstant.Path.Placeholder.id: "\(id)"
-        ]
         
-        let path = replacePlaceholders(inPath: AniListConstant.Path.SeriesGet.pageSeriesModel, withReplacingPairs: replacingPairs)
-        
-        guard let url = createAniListUrl(withPath: path, andParameters: [:]) else {
-            completionHandlerForSeries(nil, "Couldn't create AniList URL")
-            return
-        }
-        
-        let request = NSMutableURLRequest(url: url)
-        request.addValue(AniListConstant.HeaderFieldValue.contentType, forHTTPHeaderField: AniListConstant.HeaderFieldName.contentType)
-        request.addValue("Bearer \(UserDefaults.standard.string(forKey: "accessToken")!)", forHTTPHeaderField: AniListConstant.HeaderFieldName.authorization)
-        
-        let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
-            if let errorMessage = self.checkDataTaskResponseForError(data: data, response: response, error: error) {
-                completionHandlerForSeries(nil, errorMessage)
+        validateAccessToken { (errorMessage) in
+            guard errorMessage == nil else {
+                completionHandlerForSeries(nil, errorMessage!)
+                return
+            }
+         
+            let replacingPairs = [
+                AniListConstant.Path.Placeholder.seriesType: seriesType.rawValue,
+                AniListConstant.Path.Placeholder.id: "\(id)"
+            ]
+            
+            let path = self.replacePlaceholders(inPath: AniListConstant.Path.SeriesGet.pageSeriesModel, withReplacingPairs: replacingPairs)
+            
+            guard let url = self.createAniListUrl(withPath: path, andParameters: [:]) else {
+                completionHandlerForSeries(nil, "Couldn't create AniList URL")
                 return
             }
             
-            let data = data!
+            let request = NSMutableURLRequest(url: url)
+            request.addValue(AniListConstant.HeaderFieldValue.contentType, forHTTPHeaderField: AniListConstant.HeaderFieldName.contentType)
+            request.addValue("Bearer \(UserDefaults.standard.string(forKey: "accessToken")!)", forHTTPHeaderField: AniListConstant.HeaderFieldName.authorization)
             
-            guard let jsonObject = self.deserializeJson(fromData: data) else {
-                completionHandlerForSeries(nil, "Couldn't deserialize data into a JSON object")
-                return
-            }
-            
-            guard let rawSeriesDictionary = jsonObject as? [String:Any] else {
-                completionHandlerForSeries(nil, "Couldn't cast JSON to dictionary")
-                return
-            }
-            
-            typealias seriesKey = AniListConstant.ResponseKey.Series
-            
-            if seriesType.rawValue == SeriesType.anime.rawValue {
-                if let animeSeries = AnimeSeries(fromDictionary: rawSeriesDictionary) {
-                    DataSource.shared.selectedSeries = animeSeries
-                    completionHandlerForSeries(animeSeries, nil)
-                    return
-                } else {
-                    completionHandlerForSeries(nil, "Couldn't get Anime series")
+            let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
+                if let errorMessage = self.checkDataTaskResponseForError(data: data, response: response, error: error) {
+                    completionHandlerForSeries(nil, errorMessage)
                     return
                 }
-            } else {
-                if let mangaSeries = MangaSeries(fromDictionary: rawSeriesDictionary) {
-                    DataSource.shared.selectedSeries = mangaSeries
-                    completionHandlerForSeries(mangaSeries, nil)
-                    return
-                } else {
-                    completionHandlerForSeries(nil, "Couldn't get Manga series")
+                
+                let data = data!
+                
+                guard let jsonObject = self.deserializeJson(fromData: data) else {
+                    completionHandlerForSeries(nil, "Couldn't deserialize data into a JSON object")
                     return
                 }
+                
+                guard let rawSeriesDictionary = jsonObject as? [String:Any] else {
+                    completionHandlerForSeries(nil, "Couldn't cast JSON to dictionary")
+                    return
+                }
+                
+                typealias seriesKey = AniListConstant.ResponseKey.Series
+                
+                if seriesType.rawValue == SeriesType.anime.rawValue {
+                    if let animeSeries = AnimeSeries(fromDictionary: rawSeriesDictionary) {
+                        DataSource.shared.selectedSeries = animeSeries
+                        completionHandlerForSeries(animeSeries, nil)
+                        return
+                    } else {
+                        completionHandlerForSeries(nil, "Couldn't get Anime series")
+                        return
+                    }
+                } else {
+                    if let mangaSeries = MangaSeries(fromDictionary: rawSeriesDictionary) {
+                        DataSource.shared.selectedSeries = mangaSeries
+                        completionHandlerForSeries(mangaSeries, nil)
+                        return
+                    } else {
+                        completionHandlerForSeries(nil, "Couldn't get Manga series")
+                        return
+                    }
+                }
+                
             }
             
+            task.resume()
+            
         }
-        
-        task.resume()
         
     }
     
     func getPageModelCharacter(forCharacterId id: Int, completionHandlerForCharacterPageModel: @escaping (_ character: Character?, _ errorMessage: String?) -> Void) {
-        let replacingPairs = [
-            AniListConstant.Path.Placeholder.id: "\(id)"
-        ]
-        let path = replacePlaceholders(inPath: AniListConstant.Path.CharacterGet.pageCharacterModel, withReplacingPairs: replacingPairs)
-        guard let url = createAniListUrl(withPath: path, andParameters: [:]) else {
-            completionHandlerForCharacterPageModel(nil, "Couldn't create AniList URL")
-            return
-        }
         
-        let request = NSMutableURLRequest(url: url)
-        request.addValue(AniListConstant.HeaderFieldValue.contentType, forHTTPHeaderField: AniListConstant.HeaderFieldName.contentType)
-        request.addValue("Bearer \(UserDefaults.standard.string(forKey: "accessToken")!)", forHTTPHeaderField: AniListConstant.HeaderFieldName.authorization)
         
-        let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
-            if let errorMessage = self.checkDataTaskResponseForError(data: data, response: response, error: error) {
-                completionHandlerForCharacterPageModel(nil, errorMessage)
+        validateAccessToken { (errorMessage) in
+            guard errorMessage == nil else {
+                completionHandlerForCharacterPageModel(nil, errorMessage!)
+                return
+            }
+         
+            let replacingPairs = [
+                AniListConstant.Path.Placeholder.id: "\(id)"
+            ]
+            let path = self.replacePlaceholders(inPath: AniListConstant.Path.CharacterGet.pageCharacterModel, withReplacingPairs: replacingPairs)
+            guard let url = self.createAniListUrl(withPath: path, andParameters: [:]) else {
+                completionHandlerForCharacterPageModel(nil, "Couldn't create AniList URL")
                 return
             }
             
-            let data = data!
+            let request = NSMutableURLRequest(url: url)
+            request.addValue(AniListConstant.HeaderFieldValue.contentType, forHTTPHeaderField: AniListConstant.HeaderFieldName.contentType)
+            request.addValue("Bearer \(UserDefaults.standard.string(forKey: "accessToken")!)", forHTTPHeaderField: AniListConstant.HeaderFieldName.authorization)
             
-            guard let jsonObject = self.deserializeJson(fromData: data) else {
-                completionHandlerForCharacterPageModel(nil, "Couldn't deserialize data into a JSON object")
-                return
-            }
-            
-            guard let characterDictionary = jsonObject as? [String:Any],
-                let character = Character(fromDictionary: characterDictionary) else {
-                    completionHandlerForCharacterPageModel(nil, "Couldn't create character object from dictionary")
+            let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
+                if let errorMessage = self.checkDataTaskResponseForError(data: data, response: response, error: error) {
+                    completionHandlerForCharacterPageModel(nil, errorMessage)
                     return
+                }
+                
+                let data = data!
+                
+                guard let jsonObject = self.deserializeJson(fromData: data) else {
+                    completionHandlerForCharacterPageModel(nil, "Couldn't deserialize data into a JSON object")
+                    return
+                }
+                
+                guard let characterDictionary = jsonObject as? [String:Any],
+                    let character = Character(fromDictionary: characterDictionary) else {
+                        completionHandlerForCharacterPageModel(nil, "Couldn't create character object from dictionary")
+                        return
+                }
+                
+                completionHandlerForCharacterPageModel(character, nil)
+                
             }
             
-            completionHandlerForCharacterPageModel(character, nil)
+            task.resume()
+
             
         }
         
-        task.resume()
+        
         
         
     }
@@ -366,4 +397,37 @@ class AniListClient {
             return nil
         }
     }
+    
+    fileprivate func isAccessTokenValid() -> Bool {
+        let expirationTimestamp = UserDefaults.standard.integer(forKey: "expirationTimestamp")
+        let timestampNow = Int(Date().timeIntervalSince1970)
+        let isAccessTokenValid = expirationTimestamp > timestampNow
+        return isAccessTokenValid
+    }
+    
+    fileprivate func validateAccessToken(completionHandlerForValidation: @escaping (_ errorMessage: String?) -> Void) {
+        if isAccessTokenValid() {
+            completionHandlerForValidation(nil)
+            return
+        }
+        
+        getAccessToken(withRefreshToken: true) { (accessToken, _, errorMessage) in
+            guard errorMessage == nil else {
+                completionHandlerForValidation(errorMessage!)
+                return
+            }
+            
+            guard let accessToken = accessToken else {
+                completionHandlerForValidation("Couldn't get access token")
+                return
+            }
+            
+            UserDefaults.standard.set(accessToken.accessTokenValue, forKey: "accessToken")
+            UserDefaults.standard.set(accessToken.expirationTimestamp, forKey: "expirationTimestamp")
+            
+            completionHandlerForValidation(nil)
+            
+        }
+    }
+    
 }

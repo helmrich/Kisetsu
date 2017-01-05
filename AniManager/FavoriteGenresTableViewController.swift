c357abafcf845691c0713825ad20a42ee7b7c43a
Tobias Helmrich <me@tobias-helmrich.de>
2017-01-05 17:34:20 +0100

    fix: Fix bug that made pre-selected rows in the table view unselectable
    The problem that caused the bug was that just the cell's isSelected property was set
    but it wasn't actually selected by the table view. This could be fixed by instead calling
    the selectRow method on the table view and selecting the row at the appropriate index
    path

--- AniManager/FavoriteGenresTableViewController.swift
+++ AniManager/FavoriteGenresTableViewController.swift
@@ -36,22 +36,10 @@ extension FavoriteGenresTableViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "genreCell") as! FavoriteGenreTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "genreCell", for: indexPath) as! FavoriteGenreTableViewCell
         cell.textLabel?.font = UIFont(name: Constant.FontName.mainBold, size: 20.0)
         cell.textLabel?.textColor = .aniManagerBlack
         cell.textLabel?.text = DataSource.shared.genres[indexPath.row]
-        
-        
-        if let favoriteGenres = UserDefaults.standard.object(forKey: "favoriteGenres") as? [String],
-        let textLabel = cell.textLabel,
-        let text = textLabel.text {
-            if favoriteGenres.contains(text) {
-                cell.isSelected = true
-            } else {
-                cell.isSelected = false
-            }
-        }
-        
         return cell
     }
 }
@@ -65,36 +53,45 @@ extension FavoriteGenresTableViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Selected Row...")
-        if UserDefaults.standard.object(forKey: "favoriteGenres") == nil {
-            UserDefaults.standard.set([String](), forKey: "favoriteGenres")
-        }
-        
-        guard let selectedCell = tableView.cellForRow(at: indexPath),
+        /*
+            Check if the selected cell can be received from the table view
+            by getting the cell for the row at the selected row's index path
+            and unwrap the cell's text label and its text value
+         */
+        guard let selectedCell = tableView.cellForRow(at: indexPath) as? FavoriteGenreTableViewCell,
             let textLabel = selectedCell.textLabel,
             let text = textLabel.text else {
                 return
         }
         
         var selectedGenres: [String]
-        if let favoriteGenres = UserDefaults.standard.object(forKey: "favoriteGenres") as? [String] {
-            selectedGenres = favoriteGenres
-            selectedGenres.append(text)
-        } else {
-            selectedGenres = [text]
+        
+        /*
+            If the object for the "favoriteGenres" key in the user defaults
+            is nil, set an empty string array as the value for this key
+         */
+        if UserDefaults.standard.object(forKey: "favoriteGenres") == nil {
+            UserDefaults.standard.set([String](), forKey: "favoriteGenres")
         }
         
+        /*
+            Get the favorite genres object from the user defaults and cast
+            it to an array of strings. Then append the selected cell's text
+            to the array and overwrite the user defaults' object with the
+            "favoriteGenres" key with this array
+         */
+        selectedGenres = UserDefaults.standard.object(forKey: "favoriteGenres") as! [String]
+        selectedGenres.append(text)
+        
         UserDefaults.standard.set(selectedGenres, forKey: "favoriteGenres")
-        print(UserDefaults.standard.object(forKey: "favoriteGenres"))
     }
     
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        print("Deselected Row...")
         guard let favoriteGenres = UserDefaults.standard.object(forKey: "favoriteGenres") as? [String] else {
             return
         }
         
-        guard let deselectedCell = tableView.cellForRow(at: indexPath),
+        guard let deselectedCell = tableView.cellForRow(at: indexPath) as? FavoriteGenreTableViewCell,
             let textLabel = deselectedCell.textLabel,
             let text = textLabel.text else {
                 return
@@ -103,20 +100,26 @@ extension FavoriteGenresTableViewController: UITableViewDelegate {
         let updatedFavoriteGenres = favoriteGenres.filter { $0 != text }
         
         UserDefaults.standard.set(updatedFavoriteGenres, forKey: "favoriteGenres")
-        print(UserDefaults.standard.object(forKey: "favoriteGenres"))
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        /*
+            Check if favorite genres are stored in the user defaults
+            and unwrap the displayed cell's text label and its text.
+         
+            Then check if the array of favorite genres contains the
+            displayed cell's text value and if it does, select the
+            table view's row at the cell's index path
+         */
+        if let favoriteGenres = UserDefaults.standard.object(forKey: "favoriteGenres") as? [String],
+            let textLabel = cell.textLabel,
+            let text = textLabel.text {
+            if favoriteGenres.contains(text) {
+                if let _ = cell as? FavoriteGenreTableViewCell {
+                    tableView.selectRow(at: indexPath, animated: false, scrollPosition: .none)
+                }
+            }
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
